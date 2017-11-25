f4900cc43adbcde52375f8cd1b98a12bc2d3a16f
Roy Perez Jr <perezroyjr@yahoo.com>
2017-11-25 02:55:06 -0500

    Initial project commit

---
+++ CustomerDatabase/src/DataBaseGui.java
@@ -0,0 +1,11 @@
+
+public class DataBaseGui {
+
+	public static void main(String[] args) {
+
+		Customer.toString();
+		
+		
+	}
+
+}
---
+++ CustomerDatabase/src/DatabaseConnection.java
@@ -0,0 +1,10 @@
+
+public class DatabaseConnection {
+
+	
+	public static void main(String[] args){
+		
+		
+	}
+	
+}
